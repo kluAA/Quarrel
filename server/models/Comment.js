80c25cb0eba36e940b800a1aecd482a82e13365c
eqdang <elizabethqdang@gmail.com>
2020-03-02 23:56:02 -0800

    comment server model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,1 +1,19 @@
-// This is a test
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: "user"
+	},
+	question: {
+		type: Schema.Types.ObjectId,
+		ref: "question"
+	},
+	comment: {
+		type: String,
+		required: true
+	},
+});
+
+module.exports = mongoose.model("answer", CommentSchema);
