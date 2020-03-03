ddd235b4b17208577a3f68399d2ca7c64ac3bc3c
eqdang <elizabethqdang@gmail.com>
2020-03-02 23:58:11 -0800

    comment type- server schema

---
+++ server/schema/types/comment_type.js
@@ -0,0 +1,39 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList } = graphql;
+
+const mongoose = require("mongoose");
+const Comment = mongoose.model("comment");
+
+const CommentType = new GraphQLObjectType({
+	name: "CommentType",
+	fields: () => ({
+		_id: { type: GraphQLID },
+		comment: { type: GraphQLString },
+		user: {
+			type: require("./user_type"),
+			resolve(parentValue)
+			{
+				return Comment.findById(parentValue._id)
+					.populate("user")
+					.then(comment =>
+					{
+						return comment.user
+					});
+			}
+		},
+		question: {
+			type: require("./question_type"),
+			resolve(parentValue)
+			{
+				return Comment.findById(parentValue._id)
+					.populate("question")
+					.then(comment =>
+					{
+						return comment.question
+					});
+			}
+		}
+	})
+});
+
+module.exports = CommentType;
