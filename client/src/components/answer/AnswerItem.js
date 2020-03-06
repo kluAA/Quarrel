d51f8139e31756779dba0484591f4fb3e6c47560
eqdang <elizabethqdang@gmail.com>
2020-03-06 04:46:09 -0800

    revert anwertime

--- client/src/components/answer/AnswerItem.js
+++ client/src/components/answer/AnswerItem.js
@@ -10,51 +10,6 @@ class AnswerItem extends React.Component {
 		};
 	}
 
-<<<<<<< HEAD
-	// commentSection() {
-	// 	const { comments, answer } = this.props;
-	// 	if (comments.length > 0) {
-	// 		return (
-	// 			<div>
-	// 				<CommentForm answerId={this.props.answer._id} questionId={this.props.questionId} /> 
-	// 			</div>
-
-	// 		);
-	// 	} else {
-	// 		return (
-	// 			<div>
-	// 				<CommentIndex answerId={this.props.answer._id} comments={answer.comments} />
-	// 			</div>
-	// 		);
-	// 	}
-	// }
-
-	render() {
-			const { answer } = this.props;
-			return (
-					<div className="qns-answer-item">
-							<div className="ai-user-header">
-									<img className="ai-user-pic" src={answer.user.profileUrl} />
-									<div className="ai-user-details">
-											<span className="ai-user-name">{answer.user.fname} {answer.user.lname}</span>
-									</div>
-							</div>
-							<div
-									id="test"
-									contentEditable={this.state.edit}
-									className="ai-content edit-style"
-									dangerouslySetInnerHTML={{ __html: answer.body }}
-							>
-							</div>
-
-							{/* <br />
-							<p onClick={e => this.setState({edit: true})}>Toggle Edit</p> */}
-
-					{/* <CommentForm answerId={this.props.answer._id} questionId={this.props.questionId} />
-					<CommentIndex answerId={this.props.answer._id} comments={answer.comments} /> */}
-
-					{/* {this.commentSection()} */}
-=======
     render() {
         const { answer } = this.props;
         return (
@@ -77,7 +32,6 @@ class AnswerItem extends React.Component {
 							<CommentIndex answerId={this.props.answer._id} comments={answer.comments} />
                 {/* <br />
                 <p onClick={e => this.setState({edit: true})}>Toggle Edit</p> */}
->>>>>>> parent of 8055fa1... add comment to cache
 			</div>
 		)
 	}
