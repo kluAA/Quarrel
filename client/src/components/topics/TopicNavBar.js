4d0d9a97917aab1ef2279e1dbc720b09072030eb
Sam <sammygutierrez335@gmail.com>
2020-03-08 12:50:53 -0500

    adds routing for topics/name/topquestions

--- client/src/components/topics/TopicNavBar.js
+++ client/src/components/topics/TopicNavBar.js
@@ -9,8 +9,8 @@ class TopicNavBar extends React.Component {
         <div className="TopicPageNavBar">
           <ul className="list_content">
             <Link to={`/topic/${this.props.topic.name}`} className="linked_list_item">Read</Link>
-            <Link to="/topics/top_questions" className="linked_list_item">Answer</Link>
-            <Link to="/topics/writers" className="linked_list_item">Most Viewed Writers</Link>
+            <Link to={`/topic/${this.props.topic.name}/top_questions`} className="linked_list_item">Answer</Link>
+            {/* <Link to="/topics/writers" className="linked_list_item">Most Viewed Writers</Link> */}
           </ul>
         </div>
       </div>
---
+++ client/src/components/topics/TopicQuestions.js
@@ -0,0 +1,41 @@
+import React from "react";
+import TopicHeader from "./TopicHeader"
+import QuestionShow from "../questions/QuestionShow"
+import { Query } from "react-apollo";
+import Queries from "../../graphql/queries";
+const { FETCH_TOPIC_BY_NAME } = Queries;
+
+class TopicQuestions extends React.Component {
+
+  render() {
+    return (
+      < div >
+        <Query
+          query={FETCH_TOPIC_BY_NAME}
+          variables={{ name: this.props.match.params.name }}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            debugger
+            return <TopicHeader key={data.topic_by_name._id} topic={data.topic_by_name} name={data.topic_by_name.name} />
+          }}
+        </Query>
+        {/* <Query
+          query={FETCH_ANSWERS_BY_TOPIC}
+          variables={{ topicId=data.topic_by_name._id }}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            return null
+            // return <QuestionShow key={data.topic_by_name._id} topic={data.topic_by_name} name={data.topic_by_name.name} />
+          }}
+        </Query> */}
+      </div >
+
+    );
+  }
+}
+
+export default TopicQuestions;
