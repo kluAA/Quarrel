b42eb0a45aa8140da43e232894e0e42c17786720
Sammy Gutierrez <sammygutierrez335@gmail.com>
2020-04-02 16:23:43 -0500

    styles topics

--- client/src/components/topics/TopicQuestions.js
+++ client/src/components/topics/TopicQuestions.js
@@ -9,28 +9,28 @@ class TopicQuestions extends React.Component {
 
   render() {
     return (
-      < div >
         <Query
           query={FETCH_TOPIC_BY_NAME}
           variables={{ name: this.props.match.params.name }}
-        >
+          >
           {({ loading, error, data }) => {
             if (loading) return "Loading...";
             if (error) return `Error! ${error.message}`;
-            return <div>
+            return <div >
               <TopicHeader key={data.topic_by_name._id} topic={data.topic_by_name} name={data.topic_by_name.name} />
-              <div>
-                {data.topic_by_name.questions.map(question => {
+            <div className="feed-container">
 
-                  return <QuestionShow key={question._id} id={question._id} question={question} name={question.name} />
+
+                {data.topic_by_name.questions.map(question => {
+                  
+                  return <QuestionShow key={question._id} reusedComponent={true} id={question._id} question={question} name={question.name} />
                 }
                 )}
-              </div>
+
+                </div>
             </div>
           }}
         </Query>
-      </div >
-
     );
   }
 }
